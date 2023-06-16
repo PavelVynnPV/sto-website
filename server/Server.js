025ba2273801af931fc7da1edee845d2caa2b3cc
unknown <pavel.vynn@gmail.com>
2023-06-16 19:11:31 +0300

    added email to server

--- server/Server.js
+++ server/Server.js
@@ -8,7 +8,6 @@ const Comment = require("./models/Comments"); // Подключение моде
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://pavelvynnpv.github.io");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
