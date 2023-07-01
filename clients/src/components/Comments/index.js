e19e62efa0d203867629f7711da5aa97a14c237f
Pavel <92670880+PavelVynnPV@users.noreply.github.com>
2023-07-01 14:06:02 +0300

    Delete clients directory

--- clients/src/components/Comments/index.js
+++
@@ -1,2 +0,0 @@
-export {default as CommentForm} from "./CommentForm"
-export {default as CommentsList} from "./CommentsList"
--- clients/src/components/SwiperSlider/index.jsx
+++
@@ -1,2 +0,0 @@
-export {default as SwiperSlider} from "./SwiperSlider"
-export {default as SwiperBottomSlider} from "./SwiperBottomSlider"
